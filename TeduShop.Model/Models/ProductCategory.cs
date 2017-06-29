9df9bf071c6b47918b42725d6d332acc1bbfe364
SMBPY\THAONV <thaonv@smbpy.com.vn>
2017-06-29 10:11:46 +0700

    second commit

---
+++ TeduShop.Model/Models/ProductCategory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("ProductCategories")]
+    public class ProductCategory: Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+        [Required]
+        public string Name { set; get; }
+        [Required]
+        public string Alias { set; get; }
+        public string Desciption { set; get; }
+        public int? ParentID { set; get; }
+        public int? DisplayOrder { set; get; } 
+        public string Image { set; get; }
+        public bool? HomeFlag { set; get; }
+        public virtual IEnumerable<Product> Products { set; get; }
+    }
+}
